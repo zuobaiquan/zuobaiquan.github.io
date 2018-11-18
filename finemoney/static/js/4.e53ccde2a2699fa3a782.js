a26147b0d035de554c4f7ce9dec80656029bd6bc
zuobaiquan01 <zuobaiquan@126.com>
2018-11-18 17:57:17 +0800

    d

---
+++ finemoney/static/js/4.e53ccde2a2699fa3a782.js
@@ -0,0 +1,1 @@
+webpackJsonp([4],{J91o:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"index-box"})},staticRenderFns:[]},u=t("VU/8")({name:"index",data:function(){return{}},methods:{},created:function(){},mounted:function(){}},r,!1,null,null,null);n.default=u.exports}});
