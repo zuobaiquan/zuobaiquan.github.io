f58715e7c116ab9e5dcac17e113eeb0df3371707
zuobaiquan <zuobaiquan@qq.com>
2018-12-20 15:03:46 +0800

    修改

--- react2/service-worker.js
+++ react2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.471e81b5640b6cce62b1dfd523c15105.js"
+  "/react2/precache-manifest.5b4f46438089d779cf544e2ce8f7355c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/react2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
