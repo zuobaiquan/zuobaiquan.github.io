5cdbd2c11a94fdb1f559a8bd2da41e0e537e006e
zuobaiquan <zuobaiquan@qq.com>
2019-02-13 10:18:47 +0800

    update

---
+++ shangyu/precache-manifest.ce23a18e31c938a3bc97d4bb92bf86c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "97e372fe72cb50da3998",
+    "url": "/shangyu/static/js/main.chunk.js?v=97e372fe"
+  },
+  {
+    "revision": "b4a2f42b83de54cc5abb",
+    "url": "/shangyu/static/js/common/js/vendor.chunk.js?v=b4a2f42b"
+  },
+  {
+    "revision": "2c242bb2c8703ccd7161",
+    "url": "/shangyu/static/js/common/js/mainfest.chunk.js?v=2c242bb2"
+  },
+  {
+    "revision": "e49a302ca58561d9c10d",
+    "url": "/shangyu/static/js/6.chunk.js?v=e49a302c"
+  },
+  {
+    "revision": "370cfe1c1c5045161061",
+    "url": "/shangyu/static/js/5.chunk.js?v=370cfe1c"
+  },
+  {
+    "revision": "86f3bc520ac0f3274de7",
+    "url": "/shangyu/static/js/4.chunk.js?v=86f3bc52"
+  },
+  {
+    "revision": "c06fb0e2d58d3b6350a1",
+    "url": "/shangyu/static/js/3.chunk.js?v=c06fb0e2"
+  },
+  {
+    "revision": "aa5d5b80c611403bd842",
+    "url": "/shangyu/static/js/2.chunk.js?v=aa5d5b80"
+  },
+  {
+    "revision": "308fa94653bdc068d2f3",
+    "url": "/shangyu/static/js/1.chunk.js?v=308fa946"
+  },
+  {
+    "revision": "6bbde6cc44fb4a665de80a16b37b8b34",
+    "url": "/shangyu/index.html"
+  }
+];
